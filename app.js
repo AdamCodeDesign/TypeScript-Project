3ef20e6ec9c5bbef7e9ba2ccc09adf70753ab1a9
LeshchuDev <leshchudev@gmail.com>
2024-06-29 12:44:07 +0200

    union type'

--- app.js
+++ app.js
@@ -3,4 +3,19 @@ var person = {
     age: 40,
     hobbies: ["game", "sport", "chess"],
 };
-console.log(person.hobbies[0]);
+console.log(person.hobbies[2]);
+//union
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var names = combine('Adam', 'Joanna');
+var ages = combine(20, 50);
+console.log(names);
+console.log(ages);
--- app.ts
+++ app.ts
@@ -3,4 +3,22 @@ const person = {
   age: 40,
   hobbies: ["game", "sport", "chess"],
 };
-console.log(person.hobbies[0]);
+console.log(person.hobbies[2]);
+
+//union
+
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+const names = combine('Adam', 'Joanna');
+const ages = combine(20, 50);
+
+console.log(names)
+console.log(ages)
+
